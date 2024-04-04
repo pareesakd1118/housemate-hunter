ae2859cecfd1d9d1e0e1ac4b72cb1bd33bb448cd
Erin Kelley <erin.kelley01@gmail.com>
2024-04-03 19:37:36 -0600

    Add sample data

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ import sampleData from './sampledata.js';
 import FirstPage from './FirstPage'
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   const [userData, setUserData] = useState(sampleData)
+  const navigate = useNavigate();
 
   function filterByCity(event){
+    console.log(event.target.value)
     if(event.target.value === 'SF'){
+      setUserData(sampleData)
       setUserData(userData.filter(user => {
+        console.log("user city", user.city)
         return user.city === "San Francisco"}))
+        navigate("/roommates/sanfrancisco")
     } else {
+      setUserData(sampleData)
       setUserData(userData.filter(user => {
         return user.city === "Denver"}))
-    }
-
-    console.log("userData:", userData)
+        navigate("/roommates/denver")
+    } 
   }
 
   return (
--- src/FirstPage.js
+++ src/FirstPage.js
@@ -2,25 +2,23 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function FirstPage({filterByCity}){
-const [city, setCity] = useState('sanfranicso')
+const [city, setCity] = useState('')
 
 function handleSubmit(event){
     event.preventDefault()
-    setCity(event.target.value)
     filterByCity(event)
-    console.log(city)
 }
-
+console.log('City:', city)
     return (
-        <form onSubmit={event => handleSubmit(event)}>
-            <select id="selectCity" name="selectCity">
-            <option disabled selected value="">Select a City</option>
-            <option value="SF">San Francisco</option>
-            <option value="DEN">Denver</option>
+        <div>
+            <select onChange={event => handleSubmit(event)} id="selectCity" name="selectCity">
+                <option disabled selected value="">Select a City</option>
+                <option value={"SF"}>San Francisco</option>
+                <option value={"DEN"}>Denver</option>
             </select>
-            <Link to={`/roommates/${city}`}>
-                <button type='submit'>Submit</button>
-            </Link>
-        </form>
+        </div>
+
+            
+    
     )
-} 
+}           
