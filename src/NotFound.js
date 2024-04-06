d1cd8ff8ae8f83ebb20f115711d657548ab96028
Erin Kelley <erin.kelley01@gmail.com>
2024-04-06 15:50:18 -0600

    Add homepage button to error page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,11 +1,16 @@
 import Error from '../src/assets/error.png'
 import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 export default function NotFound(){
     return (
         <div className='error-container'>
             <h2 className='error-header'>404 PAGE NOT FOUND</h2>
             <img className='error' src={Error} />
+            <div>
+              <Link to={'/'}><button>Back to Homepage</button></Link>  
+            </div>
+            
         </div>
     )
 }
