795dd6cb084451c9ec26096a4ee4249fdbc12c61
Theo McCray <tlmccraycray@gmail.com>
2024-04-09 19:12:47 -0600

    Add fetching logic to RoommateDetail and adjust test to intercept Co-authored-by: <erin.kelley01@gmail.com> Co-authored-by: <pareesakd1118@gmail.com> Co-authored-by: <tlmccraycray@gmail.com>

--- src/cypress/e2e/RoommateDetail.cy.js
+++ src/cypress/e2e/RoommateDetail.cy.js
@@ -1,15 +1,11 @@
 describe('RoommateDetails Component', () => {
-    it('displays the name and image of the roommate', () => {
-      cy.visit('http://localhost:3000/roommates/details/1');
-  
-      
-    //  cy.get('.roommate-details', { timeout: 10000 }).should('be.visible');
-      cy.get('.roommate-details h2').should('contain', 'Isabella Daniels');
-      
-      cy.get('.roommate-details .profile-image img')
-        .should('be.visible')
-        .and('have.attr', 'src')
-        .and('include', 'expected_image_path');
-    });
+  it('displays the name and image of the roommate after navigation', () => {
+    cy.intercept('GET', '/api/v1/roommates/1', { fixture: 'roommate.json' }).as('getRoommateDetails');
+    cy.visit('http://localhost:3000/roommates/details/1');
+    cy.wait('@getRoommateDetails');
+    cy.get('.roommate-details h2').should('contain', 'Isabella Daniels');
+    cy.get('.roommate-details .profile-image img')
+      .should('be.visible')
+      .and('have.attr', 'src', 'https://images.unsplash.com/photo-1517462964-21fdcec3f25b?q=80&w=2640&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA');
   });
-  
+});
