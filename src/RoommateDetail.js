f7148b30f09129874def771c498add02291f9fac
pareesakd1118 <150022029+pareesakd1118@users.noreply.github.com>
2024-04-10 09:26:01 -0700

    Update RoommateDetail.js

--- src/RoommateDetail.js
+++ src/RoommateDetail.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Loading from './Loading';
 import ErrorMessage from './ErrorMessage';
+import './RoommateDetail.css'
+import Footer from './Footer'
 
 export default function RoommateDetails() {
   const [details, setDetails] = useState(null);
@@ -43,17 +45,23 @@ export default function RoommateDetails() {
 
   return (
     <div className="roommate-details">
-      <h2>{details.name}, {details.age}</h2>
-      <div className="detail"><strong>Smoker:</strong> {details.isSmoker ? 'Yes' : 'No'} </div>
-      <div className="detail"><strong>Pets:</strong> {details.hasPets ? 'Yes' : 'No'} </div>
-      <div className="detail"><strong>Max Budget: $</strong>{details.maxBudget} </div>
-      <div className="detail"> <strong>Gender:</strong> {details.gender}</div>
-      <div className="detail"> <strong>About Me:</strong> {details.bio}</div>
-      <div className="detail"> <strong>Additional Notes:</strong> {details.important}</div>
-      <div className="profile-image"> 
-          <img src={details.image} alt={`Profile pic for ${details.name}`} />
-      </div>
-    </div>
+            <div className="outer-div">
+                <div className="profile-image"> 
+                    <img className="expanded-image" src = {details.image} alt={`Profile pic for ${details.name}`}/>
+                </div>
+                <div className="inner-div">
+                    <h2>{details.name}, {details.age}</h2>
+                    <div className="detail"><strong>Smoker:</strong> {details.isSmoker ? 'Yes' : 'No'} </div>
+                    <div className="detail"><strong>Pets:</strong> {details.hasPets ? 'Yes' : 'No'} </div>
+                    <div className="detail"><strong>Max Budget: $</strong>{details.maxBudget} </div>
+                    <div className="detail"> <strong>Gender:</strong> {details.gender}</div>
+                    <div className="detail"> <strong>About Me:</strong> {details.bio}</div>
+                    <div className="detail"> <strong>Important Notes:</strong> {details.important}</div>
+                    <button className="search-btn" onClick={() => navigate(-1)}>Back to search</button>
+                </div>
+            </div>
+            <Footer />
+        </div>
   );
 }
 
